42fce8692784ef643db66646f228b63f2547198b
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-17 13:28:04 -0500

    Create repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { RecoveryPageComponent } from './auth/pages/recovery-page/recovery-page.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
